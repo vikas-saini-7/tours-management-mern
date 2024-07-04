7d05da1644860eb2aba80419a05cab61ab6a070e
vikas-saini-7 <vikas289888@gmail.com>
2024-07-04 16:49:26 +0530

    configured dotenv

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const userRouter = require('./routes/userRoutes.js');
 const app = express();
 
 // 1) MIDDLEWARES
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const app = require('./app');
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Listening...on port ${PORT}`);
 })
