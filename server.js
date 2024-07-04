49b1f129eb230fa90e620e8452c6cc424f36c273
vikas-saini-7 <vikas289888@gmail.com>
2024-07-04 21:44:14 +0530

    mongoose

--- server.js
+++ server.js
@@ -1,7 +1,45 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 const app = require('./app');
+
+console.log(process.env.MONGO_URI)
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => {console.log('DB connection successful!')})
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name']
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema)
+
+const testTour = new Tour({
+    name: 'The Forest Hiker',
+    rating: 4.7,
+    price: 497
+});
+
+testTour.save().then(doc => {
+    console.log(doc);
+}).catch(err => {
+    console.log(err);
+});
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Listening...on port ${PORT}`);
-})
+});
