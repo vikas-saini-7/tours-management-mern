3237190f4fe2a17edff19aca4d8bd38392c29fca
vikas-saini-7 <vikas289888@gmail.com>
2024-07-05 15:39:41 +0530

    created script to import and delete in mongodb

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -31,7 +31,7 @@ exports.createTour = async (req, res) => {
     } catch (err) {
         res.status(400).json({
             status: 'fail',
-            message: 'Invalid data sent!'
+            message: err
         })
     }
 }
@@ -60,24 +60,37 @@ exports.getTour = async (req, res) => {
     }
 }
 
-exports.updateTour = (req, res) => {
-    const id = req.params.id * 1;
-    // const tour = tours.find(el => el.id === id);
-
-    res.status(200).json({
-        status:"success",
-        data: {
-            tour: "<Updated tour...>"
-        }
-    })
+exports.updateTour = async (req, res) => {
+    try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        res.status(200).json({
+            status:"success",
+            data: {
+                tour: tour
+            }
+        })
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        })
+    }
 }
 
-exports.deleteTour = (req, res) => {
-    const id = req.params.id * 1;
-    // const tour = tours.find(el => el.id === id);
-
-    res.status(204).json({
-        status:"success",
-        data: null
-    })
+exports.deleteTour = async (req, res) => {
+    try {
+        const tour = await Tour.findByIdAndDelete(req.params.id);
+        res.status(204).json({
+            status:"success",
+            data: null
+        })
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        })
+    }
 }
---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const fs = require('fs');
+const mongoose = require('mongoose');
+const Tour = require('./../../models/tourModel');
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful!'));
+
+// READ JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
